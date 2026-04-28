c228d4ab92fe2ef5e1a436424aa7bbfaaee3b634
Pawan Kartik <pawankartik.chitrapu@elastic.co>
2026-04-28 22:26:00 +0100

    Add `XContent` capabilities to `CrossProjectSearchMetrics`

--- server/src/main/java/org/elasticsearch/action/search/CrossProjectSearchMetrics.java
+++ server/src/main/java/org/elasticsearch/action/search/CrossProjectSearchMetrics.java
@@ -9,12 +9,27 @@
 
 package org.elasticsearch.action.search;
 
-import java.util.HashMap;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.xcontent.ParseField;
+import org.elasticsearch.xcontent.ToXContent;
+import org.elasticsearch.xcontent.ToXContentFragment;
+import org.elasticsearch.xcontent.XContentBuilder;
 
-class CrossProjectSearchMetrics {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.TreeSet;
+
+class CrossProjectSearchMetrics implements ToXContentFragment {
     private long planningPhaseTookTime;
     private long mergingPhaseTookTime;
     private final HashMap<String, Long> projectsTookTime;
+
+    public static final ParseField PLANNING_PHASE_TOOK_TIME_FIELD = new ParseField("planning_phase_took_time");
+    public static final ParseField MERGING_PHASE_TOOK_TIME_FIELD = new ParseField("merging_phase_took_time");
+    public static final String PROJECTS_TOOK_TIME_NAME = "projects_took_time";
+    public static final String PROJECT_FIELD_NAME = "projects";
+    public static final String OVERALL_TOOK_TIME_NAME = "overall_took_time";
 
     public CrossProjectSearchMetrics() {
         this.planningPhaseTookTime = 0L;
@@ -32,5 +47,43 @@ class CrossProjectSearchMetrics {
 
     public void trackMergingPhaseTookTime(long mergingPhaseTookTime) {
         this.mergingPhaseTookTime = mergingPhaseTookTime;
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, ToXContent.Params params) throws IOException {
+        builder.field(PLANNING_PHASE_TOOK_TIME_FIELD.getPreferredName(), planningPhaseTookTime);
+        builder.field(MERGING_PHASE_TOOK_TIME_FIELD.getPreferredName(), mergingPhaseTookTime);
+
+        builder.startObject(PROJECTS_TOOK_TIME_NAME).startArray(PROJECT_FIELD_NAME);
+
+        TreeSet<String> sorted = new TreeSet<>(projectsTookTime.keySet());
+        for (String projectName : sorted) {
+            long projectTookTime = projectsTookTime.get(projectName);
+
+            builder.startObject();
+            builder.field(OVERALL_TOOK_TIME_NAME, projectTookTime);
+            builder.endObject();
+        }
+
+        builder.endArray().endObject();
+        return builder;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof CrossProjectSearchMetrics other
+            && other.planningPhaseTookTime == this.planningPhaseTookTime
+            && other.mergingPhaseTookTime == this.mergingPhaseTookTime
+            && other.projectsTookTime.equals(this.projectsTookTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(planningPhaseTookTime, mergingPhaseTookTime, projectsTookTime);
+    }
+
+    @Override
+    public String toString() {
+        return Strings.toString(this);
     }
 }
