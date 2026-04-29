4cf0ff6b2526c77ba60e21a620c23cbd192e3577
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-04-29 13:11:35 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/action/search/CrossProjectSearchMetrics.java
+++ server/src/main/java/org/elasticsearch/action/search/CrossProjectSearchMetrics.java
@@ -13,7 +13,6 @@ import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.search.profile.SearchProfileShardResult;
 import org.elasticsearch.xcontent.ParseField;
 import org.elasticsearch.xcontent.ToXContent;
 import org.elasticsearch.xcontent.ToXContentFragment;
